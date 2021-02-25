c02578a1fbdbd3c55906032171b35420b4663461
Nils Lundquist <nils@bitovi.com>
2021-02-25 15:44:04 -0700

    update build artifacts post merge

--- packages/openapi-to-graphql/lib/preprocessor.js
+++ packages/openapi-to-graphql/lib/preprocessor.js
@@ -746,17 +746,6 @@ function getSchemaName(names, usedNames) {
 /**
  * Recursively add all of the properties of an object to the data definition
  */
-function addObjectPropertiesToDataDef(def, schema, required, isInputObjectType, data, oas) {
-    /**
-     * Resolve all required properties
-     *
-     * TODO: required may contain duplicates, which is not necessarily a problem
-     */
-    if (Array.isArray(schema.required)) {
-        schema.required.forEach((requiredProperty) => {
-            required.push(requiredProperty);
-        });
-    }
 function addObjectPropertiesToDataDef(def, schema, isInputObjectType, data, oas) {
     for (let propertyKey in schema.properties) {
         let propSchemaName = propertyKey;
@@ -862,17 +851,6 @@ function resolveAllOf(schema, references, data, oas) {
                 }
                 resolvedSchema.anyOf.forEach((anyOfProperty) => {
                     collapsedSchema.anyOf.push(anyOfProperty);
-                });
-            }
-            // Collapse required if applicable
-            if ('required' in resolvedSchema) {
-                if (!('required' in collapsedSchema)) {
-                    collapsedSchema.required = [];
-                }
-                resolvedSchema.required.forEach((requiredProperty) => {
-                    if (!collapsedSchema.required.includes(requiredProperty)) {
-                        collapsedSchema.required.push(requiredProperty);
-                    }
                 });
             }
         });
